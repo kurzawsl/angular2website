a056be6d2379ae1cc2e3c33c9e2741174c7a17b3
Lukasz Kurzawski <lkurzawski@gmail.com>
2017-02-01 22:26:55 +0100

    Adding navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component }    from '@angular/core';
 
 @Component({
+  moduleId: module.id,
   selector: "my-app",
-  template: `<h1>Hello Angular</h1>`
+  templateUrl: 'app.component.html'
 })
 export class AppComponent{
-  
+
 }
