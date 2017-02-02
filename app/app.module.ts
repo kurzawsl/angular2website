2f321620b3b745d6ecd4e9f5dd7edba988cb2ff9
lukaszubs <lukasz.kurzawski@ubs.com>
2017-02-02 15:06:32 +0100

    Adding router

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,13 @@ import { BrowserModule }  from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
 import { NavbarComponent }   from './components/navbar.component';
 import { JumbotronComponent }   from './components/jumbotron/jumbotron.component';
+import { AboutComponent }   from './components/pages/about.component';
+import { HomeComponent }   from './components/pages/home.component';
+import { routing } from './app.routing'
 
 @NgModule({
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent, NavbarComponent, JumbotronComponent],
+    imports: [ BrowserModule, routing ],
+    declarations: [ AppComponent, NavbarComponent, JumbotronComponent, AboutComponent, HomeComponent],
     bootstrap: [ AppComponent ]
 })
 export class AppModule { }
